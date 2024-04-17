aa725a7f21b99f8f8b0e9e2f1e00c40d28e411fa
Janjan Flores <jan@ishkaster.com>
2024-04-17 15:09:18 +0800

    App Connection Setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware(['verify.shopify'])->name('home');
